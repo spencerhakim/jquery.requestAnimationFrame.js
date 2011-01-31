a6bd2b17868c5e2cf4e8b4cb0092e9f8a7ef73b8
louisremi <louisremi@louisremi-laptop.(none)>
2011-02-01 00:21:01 +0100

    initial commit

---
+++ jquery.requestAnimationFrame.js
@@ -0,0 +1,56 @@
+(function($, window) {
+
+$.support.requestAnimationFrame = mozRequestAnimationFrame ?
+	'mozRequestAnimationFrame' :
+	webkitRequestAnimationFrame ?
+		'webkitRequestAnimationFrame' :
+		false;
+
+var timerId;
+
+$.fx.prototype.custom = function( from, to, unit ) {
+	var self = this,
+		fx = jQuery.fx;
+
+	this.startTime = jQuery.now();
+	this.start = from;
+	this.end = to;
+	this.unit = unit || this.unit || "px";
+	this.now = this.start;
+	this.pos = this.state = 0;
+
+	function t( gotoEnd ) {
+		return self.step(gotoEnd);
+	}
+
+	t.elem = this.elem;
+
+	if ( t() && jQuery.timers.push(t) && !timerId ) {
+		jQuery.support.requestAnimationFrame ?
+			window[jQuery.support.requestAnimationFrame](fx.tick):
+			timerId = setInterval(fx.tick, fx.interval);
+	}
+};
+
+$.fx.tick = function() {
+	var timers = jQuery.timers;
+
+	for ( var i = 0; i < timers.length; i++ ) {
+		if ( !timers[i]() ) {
+			timers.splice(i--, 1);
+		}
+	}
+
+	if ( !timers.length ) {
+		jQuery.fx.stop();
+	} else if (jQuery.support.requestAnimationFrame) {
+		window[jQuery.support.requestAnimationFrame](this);
+	}
+};
+
+$.fx.stop = function() {
+	clearInterval( timerId );
+	timerId = null;
+};
+
+})(jQuery, window)
