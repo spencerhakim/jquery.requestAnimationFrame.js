5c83b0bdea3c0d5ff7ff53d0f91f1eab8764f583
Spencer Hakim <spencerhakim@gmail.com>
2011-03-02 18:50:53 -0500

    Added support for other browser-specific requestAnimationFrame functions based on Paul Irish's requestAnimFrame shim

--- jquery.requestAnimationFrame.js
+++ jquery.requestAnimationFrame.js
@@ -1,47 +1,50 @@
 (function($, window) {
 
-$.support.requestAnimationFrame = window.mozRequestAnimationFrame ?
-	'mozRequestAnimationFrame' :
-	window.webkitRequestAnimationFrame ?
-		'webkitRequestAnimationFrame' :
-		false;
+$.support.requestAnimationFrame = window.requestAnimationFrame  ? 'requestAnimationFrame' :
+          window.webkitRequestAnimationFrame                    ? 'webkitRequestAnimationFrame' :
+          window.mozRequestAnimationFrame                       ? 'mozRequestAnimationFrame' :
+          window.oRequestAnimationFrame                         ? 'oRequestAnimationFrame' :
+          window.msRequestAnimationFrame                        ? 'msRequestAnimationFrame' :
+          false;
 
 var timerId;
 
 $.fx.prototype.custom = function( from, to, unit ) {
-	var self = this,
-		fx = jQuery.fx;
+    var self = this,
+        fx = jQuery.fx;
 
-	this.startTime = jQuery.now();
-	this.start = from;
-	this.end = to;
-	this.unit = unit || this.unit || "px";
-	this.now = this.start;
-	this.pos = this.state = 0;
+    this.startTime = jQuery.now();
+    this.start = from;
+    this.end = to;
+    this.unit = unit || this.unit || "px";
+    this.now = this.start;
+    this.pos = this.state = 0;
 
-	function t( gotoEnd ) {
-		return self.step(gotoEnd);
-	}
+    function t( gotoEnd ) {
+        return self.step(gotoEnd);
+    }
 
-	t.elem = this.elem;
+    t.elem = this.elem;
 
-	if ( t() && jQuery.timers.push(t) && !timerId ) {
-		// Use requestAnimationFrame instead of setInterval if available
-		( timerId = jQuery.support.requestAnimationFrame ) ?
-			window[timerId](function raf() {
-				// timerId will be true as long as the animation hasn't been stopped
-				if (timerId) {
-					window[timerId](raf);
-					fx.tick();
-				}
-			}):
-			timerId = setInterval(fx.tick, fx.interval);
-	}
+    if ( t() && jQuery.timers.push(t) && !timerId ) {
+        // Use requestAnimationFrame instead of setInterval if available
+        ( timerId = jQuery.support.requestAnimationFrame )
+            ?
+            window[timerId](function raf() {
+                // timerId will be true as long as the animation hasn't been stopped
+                if (timerId) {
+                    window[timerId](raf);
+                    fx.tick();
+                }
+            })
+            :
+            timerId = setInterval(fx.tick, fx.interval);
+    }
 };
 
 $.fx.stop = function() {
-	clearInterval( timerId );
-	timerId = null;
+    clearInterval( timerId );
+    timerId = null;
 };
 
 })(jQuery, window)
